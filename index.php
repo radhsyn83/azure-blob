96a057142d8789b20be46e556aabbe3a6b4707cf
radhsyn83 <radhsyn83@gmail.com>
2019-05-20 10:54:44 +0700

    init

--- index.php
+++ index.php
@@ -2,4 +2,18 @@
 require_once 'vendor/autoload.php';
 require_once "./random_string.php";
 
-echo "Fathur";
+
+use MicrosoftAzure\Storage\Blob\BlobRestProxy;
+use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
+use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
+use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
+use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
+
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('radhsyn83').";AccountKey=".getenv('pK6o1K7te4zu7CqHo+2Zh48nzkoajMkLlIsCphoFBgGhWo6TBufXHeu/wE3gSW2COzfQut0LmmqjcD5T+VlqSg==');
+
+// Create blob client.
+$blobClient = BlobRestProxy::createBlobService($connectionString);
+
+$fileToUpload = "HelloWorld.txt";
+
+echo "Fathurs";
